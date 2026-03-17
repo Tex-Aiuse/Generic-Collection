cddbd142534812e5ce3575336f9869701670ba71
Tex-LeTrongTin <trongtin23102006@gmail.com>
2026-03-18 00:22:40 +0700

    Bai 17: Tim phan tu xuat hien nhieu nhat

--- Program.cs
+++ Program.cs
@@ -1,23 +1,58 @@
-﻿class BaiTap16
+﻿class BaiTap17
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Le Trong Tin - 2415053122342 - 24T3\n");
 
-        Dictionary<char, int> dict = new Dictionary<char, int>();
+        List<int> a = new List<int>();
+        int n;
 
-        Console.Write("Nhap chuoi: ");
-        string chuoi = Console.ReadLine();
+        Console.Write("Nhap so phan tu: ");
+        n = Convert.ToInt32(Console.ReadLine());
 
-        foreach (char c in chuoi)
-            if (dict.ContainsKey(c))
-                dict[c]++;
-            else
-                dict.Add(c, 1);
+        Console.WriteLine("Nhap gia tri tung phan tu:");
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            a.Add(x);
+        }
 
-        Console.WriteLine("\nSo lan xuat hien cua cac ky tu:");
+        List<int> tam = new List<int>();
+        foreach (int j in a)
+        {
+            if (tam.Contains(j) == false)
+                tam.Add(j);
+        }
 
-        foreach (var i in dict)
-            Console.Write(i.Key + " : " + i.Value + "  ");
+        int dem = 0;
+        List<int> demsl = new List<int>();
+
+        foreach (int j in tam)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] == j)
+                    dem++;
+            }
+
+            demsl.Add(dem);
+            dem = 0;
+        }
+
+        int max = demsl[0];
+        for (int i = 1; i < demsl.Count; i++)
+        {
+            if (demsl[i] > max)
+                max = demsl[i];
+        }
+
+        Console.WriteLine("\nCac so xuat hien nhieu nhat:");
+        for (int i = 0; i < demsl.Count; i++)
+        {
+            if (demsl[i] == max)
+            {
+                Console.WriteLine(tam[i] + " : " + max + " lan");
+            }
+        }
     }
 }
