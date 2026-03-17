3fc07929c9033a2d6866d32ffbc47b616a530262
Tex-LeTrongTin <trongtin23102006@gmail.com>
2026-03-18 00:03:22 +0700

    Bai 2: Tinh tong cac phan tu

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
-﻿class BaiTap1
+﻿class BaiTap2
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Le Trong Tin - 2415053122342\n");
-        int i;
-        List<int> list = new List<int>();
-        list.Add(23);
-        list.Add(10);
-        list.Add(2006);
-        list.Add(3);
-        list.Add(12);
-        Console.Write("Danh sach cac phan tu: ");
-        for (i = 0; i < list.Count; i++)
-            Console.Write("{0}, ", list[i]);
-        Console.ReadKey();
+        Console.WriteLine("Le Trong Tin - 2415053122342 - 24T3\n");
+        List<int> a = new List<int>();
+        int n;
 
+        Console.Write("Nhap so phan tu: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Nhap gia tri tung phan tu:");
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            a.Add(x);
+        }
+
+        int Tong = 0;
+
+        foreach (int j in a)
+            Tong = Tong + j;
+
+        Console.WriteLine("Tong = {0}", Tong);
     }
 }
