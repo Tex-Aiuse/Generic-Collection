3ea49fa4f2dd512cb00fd276f0ae597dc9430627
Tex-LeTrongTin <trongtin23102006@gmail.com>
2026-03-18 00:09:29 +0700

    Bai 5: Xoa phan tu khoi List

--- Program.cs
+++ Program.cs
@@ -1,29 +1,21 @@
-﻿class BaiTap4
+﻿class BaiTap5
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Le Trong Tin - 2415053122342 - 24T3\n");
-        List<int> a = new List<int>();
-        int n;
+        List<string> sv = new List<string>() {"Tin","Tung","Tu","Hai"};
 
-        Console.Write("Nhap so phan tu: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Cac sinh vien co trong danh sach: ");
+        foreach (string s in sv)
+            Console.WriteLine(s);
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("Nhap ten can xoa: ");
+        string name = Console.ReadLine();
 
-        Console.WriteLine("Nhap gia tri tung phan tu:");
-        for (int i = 0; i < n; i++)
-        {
-            int x = int.Parse(Console.ReadLine());
-            a.Add(x);
-        }
+        sv.Remove(name);
 
-        int dem = 0;
-
-        foreach (int j in a)
-        {
-            if (j % 2 == 0)
-                dem = dem + 1;
-        }
-
-        Console.WriteLine("So so chan trong danh sach la: {0}", dem);
+        Console.WriteLine("\nCac sinh vien co trong danh sach sau khi xoa: ");
+        foreach (string s in sv)
+            Console.WriteLine(s);
     }
 }
