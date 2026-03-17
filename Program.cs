4a5ce08e5efb18fad2923d197cc8e0d5ef482fca
Tex-LeTrongTin <trongtin23102006@gmail.com>
2026-03-17 23:58:29 +0700

    Bai 1: Tao danh sach so nguyen

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class BaiTap1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Le Trong Tin - 2415053122342\n");
+        int i;
+        List<int> list = new List<int>();
+        list.Add(23);
+        list.Add(10);
+        list.Add(2006);
+        list.Add(3);
+        list.Add(12);
+        Console.Write("Danh sach cac phan tu: ");
+        for (i = 0; i < list.Count; i++)
+            Console.Write("{0}, ", list[i]);
+        Console.ReadKey();
+
+    }
+}
