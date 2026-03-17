4a62dfe4d226bcfa297f6fed1d22818d9475f823
Tex-LeTrongTin <trongtin23102006@gmail.com>
2026-03-18 00:12:13 +0700

    Bai 7: Sap xem danh sach

--- Program.cs
+++ Program.cs
@@ -1,28 +1,27 @@
-﻿class BaiTap6
+﻿class BaiTap7
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Le Trong Tin - 2415053122342 - 24T3\n");
-        List<string> sv = new List<string>() { "Tin", "Tung", "Tu", "Hai" };
+        List<int> a = new List<int>();
+        int n;
 
-        Console.WriteLine("Cac sinh vien co trong danh sach: ");
-        foreach (string s in sv)
-            Console.Write("{0}, ", s);
-        Console.WriteLine("\n---------------------------------------");
-        Console.WriteLine("Nhap ten can kiem tra: ");
-        string name = Console.ReadLine();
+        Console.Write("Nhap so phan tu: ");
+        n = Convert.ToInt32(Console.ReadLine());
 
-        if (sv.Contains(name))
-            Console.WriteLine("Ton tai");
-        else
-            Console.WriteLine("Khong ton tai");
-        Console.WriteLine("\n---------------------------------------");
-        Console.WriteLine("Nhap ten can kiem tra: ");
-        name = Console.ReadLine();
+        Console.WriteLine("Nhap gia tri tung phan tu:");
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            a.Add(x);
+        }
 
-        if (sv.Contains(name))
-            Console.WriteLine("Ton tai");
-        else
-            Console.WriteLine("Khong ton tai");
+        a.Sort();
+
+        Console.WriteLine("Danh sach sau khi sap xep tang dan: ");
+        foreach (int j in a)
+        {
+            Console.Write("{0} ",j);
+        }
     }
 }
